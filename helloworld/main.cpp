290eb944bbde896c87c79b29f3ba5167f3b46b98
haolchen <haolchen@ebay.com>
2018-04-06 11:47:24 +0800

    init

---
+++ helloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Chen, Luke(AWF) on 4/6/18.
+//  Copyright © 2018 Chen, Luke(AWF). All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
