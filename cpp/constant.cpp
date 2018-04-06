228f2b420440e0391cca0fa8e1ed4c9ace2bb38a
ULIVZ <472590061@qq.com>
2018-04-06 16:38:04 +0800

    add constant

---
+++ cpp/constant.cpp
@@ -0,0 +1,38 @@
+//
+//  constant.cpp
+//  helloworld
+//
+//  Created by ULIVZ on 4/6/18.
+//  Copyright © 2018 ULIVZ. All rights reserved.
+//
+
+#include "constant.hpp"
+
+////////////////////////////////////////////
+// Integer Constant
+// 212
+// 215u    ('u' means 'unsigned')
+// 0xFeeL  ('L' means 'long')
+// 078     invalid, 8 is not a otcal number.
+// 032UU   invalid, duplicate suffix.
+
+// 85      Decimal(10)
+// 0213    Otcal(8)
+// 0x4b    Hexadecmical(16)
+// 30
+// 30u
+// 30l
+// 20ul
+//
+//
+////////////////////////////////////////////
+// Float Constant
+// 3.14159
+// 3.14149E-5L
+// 510E             // invalid, uncompleted index 'E'
+// 210f             // invalid, index or demical not found!
+// .e55             // invalid, no integer or demical
+//
+//
+////////////////////////////////////////////
+
---
+++ cpp/constant.hpp
@@ -0,0 +1,14 @@
+//
+//  constant.hpp
+//  helloworld
+//
+//  Created by Chen, Luke(AWF) on 4/6/18.
+//  Copyright © 2018 Chen, Luke(AWF). All rights reserved.
+//
+
+#ifndef constant_hpp
+#define constant_hpp
+
+#include <stdio.h>
+
+#endif /* constant_hpp */
